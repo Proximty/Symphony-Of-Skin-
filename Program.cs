35b25579c87371215001fc129ec8816d84468d22
proximty <hanna@versluizen.nl>
2026-03-10 14:31:02 +0100

    spotify doet niet leuk

--- Program.cs
+++ Program.cs
@@ -3,52 +3,44 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 using System.Text;
+using System.IO;
 using SpotifyAPI.Web;
 using SpotifyAPI.Web.Auth;
+using Newtonsoft.Json;
 
 class Program
 {
     private static readonly string clientId = "f135a36b32054ef49aac4c7e27554f85";
+    private static readonly string tokenPath = "spotify_token.json";
     private static SpotifyClient? _spotify;
 
     private static readonly Dictionary<ConsoleKey, string> GenreMap = new()
     {
-        { ConsoleKey.Spacebar, "spotify:playlist:37i9dQZF1DXcBWIGoYBM5M" }, // Jazz
-        { ConsoleKey.UpArrow,  "spotify:playlist:37i9dQZF1DX4sW2JPNYs9L" }, // Rock
-        { ConsoleKey.DownArrow, "spotify:playlist:37i9dQZF1DXcZQQ3sJ9Pga" }  // Pop
+        { ConsoleKey.Spacebar, "spotify:playlist:37i9dQZF1DXcBWIGoYBM5M" },
+        { ConsoleKey.UpArrow,  "spotify:playlist:37i9dQZF1DX4sW2JPNYs9L" },
+        { ConsoleKey.DownArrow, "spotify:playlist:37i9dQZF1DXcZQQ3sJ9Pga" }
     };
 
     static async Task Main()
     {
-        Console.WriteLine("Systeem start op... Authenticatie via browser vereist.");
-
-        var verifier = GenerateRandomString();
-        var challenge = GenerateCodeChallenge(verifier);
-
-        var server = new EmbedIOAuthServer(new Uri("http://10.17.36.151:8080/callback"), 8080);
-        await server.Start();
-
-        var loginRequest = new LoginRequest(server.BaseUri, clientId, LoginRequest.ResponseType.Code)
+        if (File.Exists(tokenPath))
         {
-            CodeChallenge = challenge,
-            CodeChallengeMethod = "S256",
-            Scope = new List<string> { Scopes.UserModifyPlaybackState, Scopes.UserReadPlaybackState }
-        };
-
-        // Handmatige URL print voor Raspberry Pi (headless)
-        Console.WriteLine("\n--- AUTHENTICATIE NODIG ---");
-        Console.WriteLine("Kopieer deze URL naar je browser op een ander apparaat:");
-        Console.WriteLine(loginRequest.ToUri());
-        Console.WriteLine("---------------------------\n");
-
-        server.AuthorizationCodeReceived += async (sender, response) =>
+            Console.WriteLine("Opgeslagen sessie gevonden. Bezig met laden...");
+            var json = File.ReadAllText(tokenPath);
+            var token = JsonConvert.DeserializeObject<PKCETokenResponse>(json);
+            
+            var authenticator = new PKCEAuthenticator(clientId, token);
+            authenticator.TokenRefreshed += (sender, t) => File.WriteAllText(tokenPath, JsonConvert.SerializeObject(t));
+            
+            _spotify = new SpotifyClient(SpotifyClientConfig.CreateDefault().WithAuthenticator(authenticator));
+            Console.WriteLine("Sessie geladen! Je kunt je Makey Makey gebruiken.");
+        }
+        else
         {
-            await server.Stop();
-            var tokenResponse = await new OAuthClient().RequestToken(new PKCETokenRequest(clientId, response.Code, server.BaseUri, verifier));
-            _spotify = new SpotifyClient(tokenResponse.AccessToken);
-            Console.WriteLine("\nSuccesvol ingelogd! Je kunt nu je Makey Makey gebruiken.");
-        };
+            await InitialAuth();
+        }
 
+        // Loop voor Makey Makey input
         while (true)
         {
             var keyInfo = Console.ReadKey(intercept: true);
@@ -60,31 +52,42 @@ class Program
         }
     }
 
-    static string GenerateRandomString()
+    static async Task InitialAuth()
     {
-        byte[] randomBytes = new byte[32];
-        using (var rng = RandomNumberGenerator.Create()) { rng.GetBytes(randomBytes); }
-        return Convert.ToBase64String(randomBytes).Replace("+", "-").Replace("/", "_").Replace("=", "");
+        var verifier = GenerateRandomString();
+        var challenge = GenerateCodeChallenge(verifier);
+
+        // Gebruik 'localhost' voor de redirect, gebruik SSH-tunnel op laptop als dit faalt
+        var server = new EmbedIOAuthServer(new Uri("http://localhost:8080/callback"), 8080);
+        await server.Start();
+
+        var loginRequest = new LoginRequest(server.BaseUri, clientId, LoginRequest.ResponseType.Code)
+        {
+            CodeChallenge = challenge,
+            CodeChallengeMethod = "S256",
+            Scope = new List<string> { Scopes.UserModifyPlaybackState, Scopes.UserReadPlaybackState }
+        };
+
+        Console.WriteLine("\n--- AUTHENTICATIE NODIG (SLECHTS ÉÉN KEER) ---");
+        Console.WriteLine("Open deze link op je laptop (gebruik SSH tunnel indien nodig):");
+        Console.WriteLine(loginRequest.ToUri());
+
+        server.AuthorizationCodeReceived += async (sender, response) =>
+        {
+            await server.Stop();
+            var tokenResponse = await new OAuthClient().RequestToken(new PKCETokenRequest(clientId, response.Code, server.BaseUri, verifier));
+            
+            File.WriteAllText(tokenPath, JsonConvert.SerializeObject(tokenResponse));
+            
+            var authenticator = new PKCEAuthenticator(clientId, tokenResponse);
+            _spotify = new SpotifyClient(SpotifyClientConfig.CreateDefault().WithAuthenticator(authenticator));
+            
+            Console.WriteLine("\nSuccesvol gekoppeld! Token is opgeslagen.");
+        };
     }
 
-    static string GenerateCodeChallenge(string verifier)
-    {
-        using var sha256 = SHA256.Create();
-        byte[] challengeBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(verifier));
-        return Convert.ToBase64String(challengeBytes).Replace("+", "-").Replace("/", "_").Replace("=", "");
-    }
-
-    static async Task SpeelMuziek(string playlistUri)
-    {
-        try 
-        {
-            var resumeRequest = new PlayerResumePlaybackRequest { ContextUri = playlistUri };
-            await _spotify.Player.ResumePlayback(resumeRequest);
-            Console.WriteLine($"Muziek gestart: {playlistUri}");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Fout: {ex.Message}. Staat Spotify wel open op je Pi (Raspotify)?");
-        }
-    }
+    // ... (GenerateRandomString, GenerateCodeChallenge en SpeelMuziek blijven hetzelfde)
+    static string GenerateRandomString() { /* ... */ return ""; } // Vul je bestaande logica hier in
+    static string GenerateCodeChallenge(string verifier) { /* ... */ return ""; }
+    static async Task SpeelMuziek(string playlistUri) { /* ... */ }
 }
