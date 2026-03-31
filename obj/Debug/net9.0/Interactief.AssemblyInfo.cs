5d7653a919271f1a372dc86a073d69f3097bfe89
proximty <hanna@versluizen.nl>
2026-03-31 11:03:14 +0200

    back to before test

--- obj/Debug/net9.0/Interactief.AssemblyInfo.cs
+++ obj/Debug/net9.0/Interactief.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Interactief")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+212c47015edd74558f7e01a5a21ecc265f4095c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cf98612f368ed076865c9b5b9e22dad00361663")]
 [assembly: System.Reflection.AssemblyProductAttribute("Interactief")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Interactief")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
